096ba901b627a2cbb2cb5dd915e8fb3bf6b874dd
Dylan Nguyen <dylanminhnguyen@gmail.com>
2017-11-14 20:23:33 -0800

    Basic texture loading using terrain.png

--- CS445Craft/src/cs445craft/Voxel.java
+++ CS445Craft/src/cs445craft/Voxel.java
@@ -13,7 +13,15 @@
 ****************************************************************/
 package cs445craft;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import static org.lwjgl.opengl.GL11.*;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
 
 public class Voxel extends Drawable {
     protected float r = .5f;
@@ -26,6 +34,20 @@ public class Voxel extends Drawable {
         super(x,y,z);
         this.r = r;
     }
+    /**
+     * method: loadTexture
+     * purpose: Use the TextureLoader class to load in the texture from a file
+     */
+    public Texture loadTexture(String key) {
+        try {
+            return TextureLoader.getTexture("png", new FileInputStream(new File("res/" + key + ".png")));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Screen.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Screen.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
     
     /**
     * method: draw
@@ -33,53 +55,95 @@ public class Voxel extends Drawable {
     * equal to 2r.
     **/
     public void draw() {
+        Texture terrain = loadTexture("terrain");
         glPushMatrix();
         glTranslatef(x, y, z);
-        glBegin(GL_QUADS);
         
+        terrain.bind();
+        
+        glBegin(GL_QUADS);
+                
         // top face
-	glColor3f(1.0f, 0.0f, 0.0f);
+//	glColor3f(1.0f, 0.0f, 0.0f);
+        glTexCoord2f(0,0);
 	glVertex3f(-r, r, r);
+        glTexCoord2f(0,1);
 	glVertex3f(r, r, r);
+        glTexCoord2f(1,1);
 	glVertex3f(r, r, -r);
+        glTexCoord2f(1,0);
 	glVertex3f(-r, r, -r);
         
         // bottom face
-        glColor3f(1.0f, 1.0f, 0.0f);
+//        glColor3f(1.0f, 1.0f, 0.0f);
+        glTexCoord2f(0,0);
 	glVertex3f(r, -r, -r);
+        glTexCoord2f(0,1);
 	glVertex3f(-r, -r, -r);
+        glTexCoord2f(1,1);
 	glVertex3f(-r, -r, r);
+        glTexCoord2f(1,0);
 	glVertex3f(r, -r, r);
+        
  
 	// front face
-	glColor3f(0.0f, 1.0f, 0.0f);
+//	glColor3f(0.0f, 1.0f, 0.0f);
+        glTexCoord2f(0,0);
 	glVertex3f(r, -r, r);
+        glTexCoord2f(0,1);
 	glVertex3f(r, r, r);
+        glTexCoord2f(1,1);
 	glVertex3f(-r, r, r);
+        glTexCoord2f(1,0);
 	glVertex3f(-r, -r, r);
+        
         
         // back face
-        glColor3f(0.0f, 1.0f, 1.0f);
+//        glColor3f(0.0f, 1.0f, 1.0f);
+        glTexCoord2f(0,0);
 	glVertex3f(-r, r, -r);
+        glTexCoord2f(0,1);
 	glVertex3f(-r, -r, -r);
+        glTexCoord2f(1,1);
 	glVertex3f(r, -r, -r);
+        glTexCoord2f(1,0);
 	glVertex3f(r, r, -r);
+        
  
 	// right face
-	glColor3f(0.0f, 0.0f, 1.0f);
+//	glColor3f(0.0f, 0.0f, 1.0f);
+        glTexCoord2f(0,0);
 	glVertex3f(r, r, -r);
+        glTexCoord2f(0,1);
 	glVertex3f(r, r, r);
+        glTexCoord2f(1,1);
 	glVertex3f(r, -r, r);
+        glTexCoord2f(1,0);
 	glVertex3f(r, -r, -r);
         
+        
         // left face
-	glColor3f(1.0f, 0.0f, 1.0f);
+//	glColor3f(1.0f, 0.0f, 1.0f);
+        glTexCoord2f(0,0);
 	glVertex3f(-r, -r, r);
+        glTexCoord2f(0,1);
 	glVertex3f(-r, -r, -r);
+        glTexCoord2f(1,1);
 	glVertex3f(-r, r, -r);
+        glTexCoord2f(1,0);
 	glVertex3f(-r, r, r);
+        
         
         glEnd();
         glPopMatrix();
+    }
+    
+    /**
+     * method: chooseTexture
+     * purpose: based on block location (which should be passed in as a parameter)
+     * the the proper texture coordinates for the relevant block will be applied
+     */
+    public void chooseTexture() {
+        // texture logic
     }
 }

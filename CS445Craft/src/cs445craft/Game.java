8adabf699461bf73c5359837da0f962fd1dff384
Christian Hill <Christian.T.Hill@jpl.nasa.gov>
2017-11-19 20:27:04 -0800

    cleanup Game.java

--- CS445Craft/src/cs445craft/Game.java
+++ CS445Craft/src/cs445craft/Game.java
@@ -86,217 +86,243 @@ public class Game {
             // move mouse to center
             Mouse.setCursorPosition(0,0);
             
-            // listen for left click
-            if (Mouse.isButtonDown(0)) {
-                if (!lastLeftMouseState) {
-                    lastLeftMouseState = true;
-                    
-                    float clickX = (float) (Math.sin(Math.toRadians(camera.yaw)) * Math.cos(Math.toRadians(-camera.pitch)));
-                    float clickZ = (float) (Math.cos(Math.toRadians(camera.yaw)) * Math.cos(Math.toRadians(-camera.pitch)));
-                    float clickY = (float)  Math.sin(Math.toRadians(-camera.pitch));
-                    
-                    for (float amplitude = 0.5f; amplitude <= Voxel.BLOCK_SIZE * 2; amplitude++) {
-                        float projectX = camera.x + amplitude * clickX;
-                        float projectZ = camera.z - amplitude * clickZ;
-                        float projectY = camera.y + amplitude * clickY;
-                        
-                        Voxel.VoxelType clickedBlock = world.blockAt(projectX, projectY, projectZ);
-                        if (clickedBlock == null || Voxel.isMineThrough(clickedBlock)) {
-                            continue;
-                        }
-                        
-                        // check if block can be broken
-                        if (Voxel.isBreakable(clickedBlock)) {
-                            world.removeBlock(projectX, projectY, projectZ);
-                            break;
-                        } else {
-                            break;
-                        }
-                    } 
-                }
-            } else {
-                lastLeftMouseState = false;
-            }
+            // update world position
+            updateWorldPos();
             
-            boolean gridPositionUpdated = false;
-            boolean chunkPositionUpdated = false;
-            if (world.worldPosToBlockIndex(camera.x) != worldX) {
-                gridPositionUpdated = true;
-                worldX = world.worldPosToBlockIndex(camera.x);
-            }
+            // handle mouse events (mouse movement & clicks)
+            mouseEvents();
             
-            if (world.worldPosToBlockIndex(camera.z) != worldZ) {
-                gridPositionUpdated = true;
-                worldZ = world.worldPosToBlockIndex(camera.z);
-            }
-            
-            if (world.blockIndexToChunkNum(worldX) != chunkI) {
-                chunkPositionUpdated = true;
-                chunkI = world.blockIndexToChunkNum(worldX);
-            }
-            
-            if (world.blockIndexToChunkNum(worldZ) != chunkJ) {
-                chunkPositionUpdated = true;
-                chunkJ = world.blockIndexToChunkNum(worldZ);
-            }
-            
-            if (gridPositionUpdated || chunkPositionUpdated) {
-                System.out.println("pos (" + worldX + "," + worldZ + ") chunk (" + chunkI + "," + chunkJ + ")");
-            }
-            
-            if (chunkPositionUpdated) {
-                List<Runnable> chunkGenerationTasks = worldGen.newChunkPosition(chunkI, chunkJ, screen);
-                taskQueue.addTasks(chunkGenerationTasks);
-            }
-            
-            if (Keyboard.isKeyDown(Keyboard.KEY_V)) {
-                if (!lastVState) {
-                    lastVState = true;
-                    noClip = !noClip;
-                }
-            } else {
-                lastVState = false;
-            }
-            
-            if (Keyboard.isKeyDown(Keyboard.KEY_R)) {
-                if (!lastRState) {
-                    lastRState = true;
-                    world.swapMeshes();
-                }
-            } else {
-                lastRState = false;
-            }
-            
-            float speed = MOVEMENT_SPEED;
-            if (noClip) {
-                speed = NOCLIP_SPEED;
-            }
-            
-            float dx = 0.0f;
-            float dz = 0.0f;
-            float dy = yspeed;
-   
-            // gravity and jumping
-            // listen for jump
-            boolean blockBelow = world.solidBlockAt(camera.x, camera.y + dy - PLAYER_HEIGHT, camera.z) != null;
-            boolean blockAbove = world.solidBlockAt(camera.x, camera.y + dy + 0.5f, camera.z) != null;
-            
-            if (noClip) {
-                // skip gravity code if noclip is enabled
-                if (Keyboard.isKeyDown(Keyboard.KEY_SPACE))
-                    camera.y += speed;
-                if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))
-                    camera.y -= speed;
-                dy = 0;
-                yspeed = 0;
-            }  else if (blockBelow) {
-                yspeed = 0;
-                dy = 0;
-                // prevent player from getting stuck in floor if they have high y speed
-                camera.y = world.depthAt(camera.x, camera.y, camera.z) + PLAYER_HEIGHT + 0.75f;
-                //camera.y = Math.round(camera.y - 0.5f);
-            } else {
-                if (blockAbove && yspeed > 0) {
-                    yspeed = 0;
-                }
-                
-                // accelerate on the yaxis for the next frame
-                yspeed -= GRAVITY;
-            }
-            
-            if (yspeed < -TERMINAL_VELOCITY) {
-                yspeed = -TERMINAL_VELOCITY;
-            } else if (yspeed > TERMINAL_VELOCITY) {
-                yspeed = TERMINAL_VELOCITY;
-            }
-            
-            if (Keyboard.isKeyDown(Keyboard.KEY_SPACE) && blockBelow && !noClip) {
-                if (!lastSpaceState) {
-                    lastSpaceState = true;
-                    yspeed = JUMP_SPEED;
-                }
-            } else {
-                lastSpaceState = false;
-            }
+            // handle all keyboard events
+            keyboardEvents();
 
-            // world movement            
-            if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
-                dx += speed * (float) Math.sin(Math.toRadians(camera.yaw));
-                dz += -speed * (float) Math.cos(Math.toRadians(camera.yaw));
-            } else if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
-                dx += -speed * (float) Math.sin(Math.toRadians(camera.yaw));
-                dz += speed * (float) Math.cos(Math.toRadians(camera.yaw));
-            }
-            
-            if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
-                dx += speed * (float)Math.sin(Math.toRadians(camera.yaw-90));
-                dz += -speed * (float)Math.cos(Math.toRadians(camera.yaw-90));
-            } else if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
-                dx += -speed * (float)Math.sin(Math.toRadians(camera.yaw-90));
-                dz += speed * (float)Math.cos(Math.toRadians(camera.yaw-90));
-            }
-            
-            float offsetX, offsetZ, offsetXZ;
-            offsetX = offsetZ = 0.0f;
-            offsetXZ = Voxel.BLOCK_SIZE / 3.75f;
-            
-            if (dx > 0) {
-                offsetX = offsetXZ;
-            } else if (dx < 0) {
-                offsetX = -offsetXZ;
-            }
-            
-            if (dz > 0) {
-                offsetZ = offsetXZ;
-            } else if (dz < 0) {
-                offsetZ = -offsetXZ;
-            }
-
-            // player is ~2 blocks tall, so we must check side collision twice on each axis
-            float offsetY = PLAYER_HEIGHT * SIDE_COLLIDE_HEIGHT_FACTOR;
-            boolean willCollideX = world.solidBlockAt(camera.x + dx + offsetX, camera.y - offsetY, camera.z) != null;
-                   willCollideX |= world.solidBlockAt(camera.x + dx + offsetX, camera.y - offsetY + Voxel.BLOCK_SIZE, camera.z) != null;
-            boolean willCollideZ = world.solidBlockAt(camera.x, camera.y - offsetY, camera.z + dz + offsetZ) != null;
-                   willCollideZ |= world.solidBlockAt(camera.x, camera.y - offsetY + Voxel.BLOCK_SIZE, camera.z + dz + offsetZ) != null;
-                   
-            // special case for when running directly into a corner
-            boolean willCollideXZ = world.solidBlockAt(camera.x + dx + offsetX, camera.y - offsetY, camera.z + dz + offsetZ) != null;
-                   willCollideXZ |= world.solidBlockAt(camera.x + dx + offsetX, camera.y - offsetY + Voxel.BLOCK_SIZE, camera.z + dz + offsetZ) != null;
-                   
-            if (willCollideXZ && !willCollideX && !willCollideZ && !noClip) {
-                dx = 0;
-                dz = 0;
-            }
-                    
-            if (willCollideX && !noClip) {
-                dx = 0;
-            }
-            if (willCollideZ && !noClip) {
-                dz = 0;
-            }
-            
-            // world movement
-            camera.x += dx;
-            camera.z += dz;
-            camera.y += dy;
             
             // check if underwater
-            Voxel.VoxelType blockAtCamera = world.blockAt(camera.x, camera.y, camera.z);
-            if (blockAtCamera == Voxel.VoxelType.WATER) {
-                screen.setTintColor(0.75f, 0.75f, 1.0f);
-            } else {
-                screen.setTintColor(1.0f, 1.0f, 1.0f);
-            }
-            
-            // look movement
-            camera.incYaw(Mouse.getDX() * MOUSE_SENS);
-            camera.incPitch(Mouse.getDY() * MOUSE_SENS);
+            checkPlayerStatus();
 
             // run one async task
             taskQueue.run(1);
             
             // draw frame
             screen.drawFrame();
+        }
+    }
+    
+    private void updateWorldPos() {
+        boolean gridPositionUpdated = false;
+        boolean chunkPositionUpdated = false;
+        if (world.worldPosToBlockIndex(camera.x) != worldX) {
+            gridPositionUpdated = true;
+            worldX = world.worldPosToBlockIndex(camera.x);
+        }
+
+        if (world.worldPosToBlockIndex(camera.z) != worldZ) {
+            gridPositionUpdated = true;
+            worldZ = world.worldPosToBlockIndex(camera.z);
+        }
+
+        if (world.blockIndexToChunkNum(worldX) != chunkI) {
+            chunkPositionUpdated = true;
+            chunkI = world.blockIndexToChunkNum(worldX);
+        }
+
+        if (world.blockIndexToChunkNum(worldZ) != chunkJ) {
+            chunkPositionUpdated = true;
+            chunkJ = world.blockIndexToChunkNum(worldZ);
+        }
+
+        if (gridPositionUpdated || chunkPositionUpdated) {
+            System.out.println("pos (" + worldX + "," + worldZ + ") chunk (" + chunkI + "," + chunkJ + ")");
+        }
+
+        if (chunkPositionUpdated) {
+            List<Runnable> chunkGenerationTasks = worldGen.newChunkPosition(chunkI, chunkJ, screen);
+            taskQueue.addTasks(chunkGenerationTasks);
+        }
+    }
+    
+    private void mouseEvents() {
+        // look movement
+        camera.incYaw(Mouse.getDX() * MOUSE_SENS);
+        camera.incPitch(Mouse.getDY() * MOUSE_SENS);
+        
+        // listen for left click
+        if (Mouse.isButtonDown(0)) {
+            if (!lastLeftMouseState) {
+                lastLeftMouseState = true;
+
+                float clickX = (float) (Math.sin(Math.toRadians(camera.yaw)) * Math.cos(Math.toRadians(-camera.pitch)));
+                float clickZ = (float) (Math.cos(Math.toRadians(camera.yaw)) * Math.cos(Math.toRadians(-camera.pitch)));
+                float clickY = (float)  Math.sin(Math.toRadians(-camera.pitch));
+
+                for (float amplitude = 0.5f; amplitude <= Voxel.BLOCK_SIZE * 2; amplitude++) {
+                    float projectX = camera.x + amplitude * clickX;
+                    float projectZ = camera.z - amplitude * clickZ;
+                    float projectY = camera.y + amplitude * clickY;
+
+                    Voxel.VoxelType clickedBlock = world.blockAt(projectX, projectY, projectZ);
+                    if (clickedBlock == null || Voxel.isMineThrough(clickedBlock)) {
+                        continue;
+                    }
+
+                    // check if block can be broken
+                    if (Voxel.isBreakable(clickedBlock)) {
+                        world.removeBlock(projectX, projectY, projectZ);
+                        break;
+                    } else {
+                        break;
+                    }
+                } 
+            }
+        } else {
+            lastLeftMouseState = false;
+        }
+    }
+    
+    private void keyboardEvents() {
+        if (Keyboard.isKeyDown(Keyboard.KEY_V)) {
+            if (!lastVState) {
+                lastVState = true;
+                noClip = !noClip;
+            }
+        } else {
+            lastVState = false;
+        }
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_R)) {
+            if (!lastRState) {
+                lastRState = true;
+                world.swapMeshes();
+            }
+        } else {
+            lastRState = false;
+        }
+
+        worldMovement();
+    }
+    
+    private void worldMovement() {
+        float dx = 0.0f;
+        float dz = 0.0f;
+        float dy = yspeed;
+
+        // gravity and jumping
+        // listen for jump
+        boolean blockBelow = world.solidBlockAt(camera.x, camera.y + dy - PLAYER_HEIGHT, camera.z) != null;
+        boolean blockAbove = world.solidBlockAt(camera.x, camera.y + dy + 0.5f, camera.z) != null;
+
+        if (noClip) {
+            // skip gravity code if noclip is enabled
+            if (Keyboard.isKeyDown(Keyboard.KEY_SPACE))
+                camera.y += NOCLIP_SPEED;
+            if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))
+                camera.y -= NOCLIP_SPEED;
+            dy = 0;
+            yspeed = 0;
+        }  else if (blockBelow) {
+            yspeed = 0;
+            dy = 0;
+            // prevent player from getting stuck in floor if they have high y speed
+            camera.y = world.depthAt(camera.x, camera.y, camera.z) + PLAYER_HEIGHT + 0.75f;
+            //camera.y = Math.round(camera.y - 0.5f);
+        } else {
+            if (blockAbove && yspeed > 0) {
+                yspeed = 0;
+            }
+
+            // accelerate on the yaxis for the next frame
+            yspeed -= GRAVITY;
+        }
+
+        if (yspeed < -TERMINAL_VELOCITY) {
+            yspeed = -TERMINAL_VELOCITY;
+        } else if (yspeed > TERMINAL_VELOCITY) {
+            yspeed = TERMINAL_VELOCITY;
+        }
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_SPACE) && blockBelow && !noClip) {
+            if (!lastSpaceState) {
+                lastSpaceState = true;
+                yspeed = JUMP_SPEED;
+            }
+        } else {
+            lastSpaceState = false;
+        }
+        
+        float speed = MOVEMENT_SPEED;
+        if (noClip) {
+            speed = NOCLIP_SPEED;
+        }
+        
+        // world movement            
+        if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
+            dx += speed * (float) Math.sin(Math.toRadians(camera.yaw));
+            dz += -speed * (float) Math.cos(Math.toRadians(camera.yaw));
+        } else if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
+            dx += -speed * (float) Math.sin(Math.toRadians(camera.yaw));
+            dz += speed * (float) Math.cos(Math.toRadians(camera.yaw));
+        }
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
+            dx += speed * (float)Math.sin(Math.toRadians(camera.yaw-90));
+            dz += -speed * (float)Math.cos(Math.toRadians(camera.yaw-90));
+        } else if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
+            dx += -speed * (float)Math.sin(Math.toRadians(camera.yaw-90));
+            dz += speed * (float)Math.cos(Math.toRadians(camera.yaw-90));
+        }
+
+        float offsetX, offsetZ, offsetXZ;
+        offsetX = offsetZ = 0.0f;
+        offsetXZ = Voxel.BLOCK_SIZE / 3.75f;
+
+        if (dx > 0) {
+            offsetX = offsetXZ;
+        } else if (dx < 0) {
+            offsetX = -offsetXZ;
+        }
+
+        if (dz > 0) {
+            offsetZ = offsetXZ;
+        } else if (dz < 0) {
+            offsetZ = -offsetXZ;
+        }
+
+        // player is ~2 blocks tall, so we must check side collision twice on each axis
+        float offsetY = PLAYER_HEIGHT * SIDE_COLLIDE_HEIGHT_FACTOR;
+        boolean willCollideX = world.solidBlockAt(camera.x + dx + offsetX, camera.y - offsetY, camera.z) != null;
+               willCollideX |= world.solidBlockAt(camera.x + dx + offsetX, camera.y - offsetY + Voxel.BLOCK_SIZE, camera.z) != null;
+        boolean willCollideZ = world.solidBlockAt(camera.x, camera.y - offsetY, camera.z + dz + offsetZ) != null;
+               willCollideZ |= world.solidBlockAt(camera.x, camera.y - offsetY + Voxel.BLOCK_SIZE, camera.z + dz + offsetZ) != null;
+
+        // special case for when running directly into a corner
+        boolean willCollideXZ = world.solidBlockAt(camera.x + dx + offsetX, camera.y - offsetY, camera.z + dz + offsetZ) != null;
+               willCollideXZ |= world.solidBlockAt(camera.x + dx + offsetX, camera.y - offsetY + Voxel.BLOCK_SIZE, camera.z + dz + offsetZ) != null;
+
+        if (willCollideXZ && !willCollideX && !willCollideZ && !noClip) {
+            dx = 0;
+            dz = 0;
+        }
+
+        if (willCollideX && !noClip) {
+            dx = 0;
+        }
+        if (willCollideZ && !noClip) {
+            dz = 0;
+        }
+
+        // world movement
+        camera.x += dx;
+        camera.z += dz;
+        camera.y += dy;
+    }
+    
+    private void checkPlayerStatus() {
+        Voxel.VoxelType blockAtCamera = world.blockAt(camera.x, camera.y, camera.z);
+        
+        // check if underwater
+        if (blockAtCamera == Voxel.VoxelType.WATER) {
+            screen.setTintColor(0.75f, 0.75f, 1.0f);
+        } else {
+            screen.setTintColor(1.0f, 1.0f, 1.0f);
         }
     }
 }
